7cb9b32a3f9d6c74eac9b1b24cc603e63fd08921
Bjørn Lau Jørgensen <bjornlau@bjrns-mbp.mynet>
2019-12-09 09:28:27 +0100

    Initial commit

---
+++ Gridly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gridly
+//
+//  Created by Bjørn Lau Jørgensen on 09/12/2019.
+//  Copyright © 2019 Bjørn Lau Jørgensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
